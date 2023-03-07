895e0f530b5938d008f4b934bdea4de1f89b0859
alex <907785012@qq.com>
2023-03-07 21:23:10 +0800

    Add a create component and set exact router path

---
+++ src/Create.js
@@ -0,0 +1,10 @@
+const Create = () => {
+    return (
+        <div className="idol">
+            <h2>Add new blog</h2>
+
+            <img src="https://s2.loli.net/2023/03/07/X8qGA7usSk6Parh.jpg" alt="soo-jin" />
+        </div>
+    );
+}
+export default Create;
