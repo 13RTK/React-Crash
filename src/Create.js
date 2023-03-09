1f7e819453ef04fba965c0c691a3c632ba0fef07
alex <907785012@qq.com>
2023-03-09 22:52:28 +0800

    Bind the user input with the state variables in Create component

--- src/Create.js
+++ src/Create.js
@@ -1,9 +1,46 @@
-const Create = () => {
-    return (
-        <div className="idol">
-            <h2>Add new blog</h2>
+import { useState } from "react";
 
-            <img src="https://s2.loli.net/2023/03/07/X8qGA7usSk6Parh.jpg" alt="soo-jin" />
+const Create = () => {
+    const [title, setTitle] = useState("");
+    const [author, setAuthor] = useState("");
+    const [body, setBody] = useState("");
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        const blog = { title, body, author };
+        console.log(blog);
+    }
+
+    return (
+        <div className="create">
+            <h2>Add new blog</h2>
+            <form onSubmit={handleSubmit}>
+                <label>Blog title:</label>
+                <input
+                    type="text"
+                    required
+                    value={title}
+                    onChange={(e) => setTitle(e.target.value)}
+                />
+
+                <label>Blog body:</label>
+                <textarea
+                    required
+                    value={body}
+                    onChange={(e) => setBody(e.target.value)}
+                ></textarea>
+
+                <label>Blog author:</label>
+                <select
+                    value={author}
+                    onChange={(e) => setAuthor(e.target.value)}
+                >
+                    <option value="Alex">Alex</option>
+                    <option value="Sasha">Sasha</option>
+                </select>
+                <button>Add blog</button>
+            </form>
         </div>
     );
 }
